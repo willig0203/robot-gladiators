e4e8860c75423db7ca16ad58269922b723841979
willig0203 <100536076+willig0203@users.noreply.github.com>
2022-05-13 16:14:29 -0500

    fixed name bug

--- assets/js/game.js
+++ assets/js/game.js
@@ -144,8 +144,19 @@ var randomNumber = function(min, max) {
   return value;
 };
 
+var getPlayerName = function() {
+  var name = "";
+
+  while (name === "" || name === null) {
+    name = prompt("What is your robot's name?");
+  }
+
+  console.log("Your robot's name is " + name);
+  return name;
+};
+
 var playerInfo = {
-  name: window.prompt("What is your robot's name?"),
+  name: getPlayerName(),
   health: 100,
   attack: 10,
   money: 10,
@@ -192,6 +203,5 @@ var enemyInfo = [
     attack: randomNumber(10, 14)
   }
 ];
-
 
 startGame();
