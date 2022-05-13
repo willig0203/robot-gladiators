827fdfce5b663f1ffaec8b022c8ca78bb8b837bf
willig0203 <100536076+willig0203@users.noreply.github.com>
2022-05-13 17:24:09 -0500

    mess

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,3 +1,4 @@
+/* GAME FUNCTIONS */
 
 var fightOrSkip = function() {
   var promptFight = window.prompt('Would you like to FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.');
@@ -24,88 +25,102 @@ var fight = function(enemy) {
       break;
     }
 
+    // generate random damage value based on player's attack power
     var damage = randomNumber(playerInfo.attack - 3, playerInfo.attack);
 
-    enemyHealth = Math.max(0, enemyHealth - damage);
+    enemy.health = Math.max(0, enemy.health - damage);
     console.log(
-      playerInfo.name + ' attacked ' + enemy.name + '. ' + enemy.name + ' now has ' + enemyHealth + ' health remaining.'
+      playerInfo.name + ' attacked ' + enemy.name + '. ' + enemy.name + ' now has ' + enemy.health + ' health remaining.'
     );
 
-    if (enemyHealth <= 0) {
-      window.alert(enemy + ' has died!');
+    // check enemy's health
+    if (enemy.health <= 0) {
+      window.alert(enemy.name + ' has died!');
 
-      playerInfo.money = Math.max(0, playerInfo.money - 10);
-  
+      // award player money for winning
+      playerInfo.money = playerInfo.money + 20;
+
+      // leave while() loop since enemy is dead
       break;
     } else {
-      window.alert(enemy.name + ' still has ' + enemyHealth + ' health left.');
+      window.alert(enemy.name + ' still has ' + enemy.health + ' health left.');
     }
 
+    // remove players's health by subtracting the amount set in the enemy.attack variable
     var damage = randomNumber(enemy.attack - 3, enemy.attack);
 
     playerInfo.health = Math.max(0, playerInfo.health - damage);
+    
     console.log(
       enemy.name + ' attacked ' + playerInfo.name + '. ' + playerInfo.name + ' now has ' + playerInfo.health + ' health remaining.'
     );
 
-  
+    // check player's health
     if (playerInfo.health <= 0) {
-      window.alert(playerName + ' has died!');
-  
+      window.alert(playerInfo.name + ' has died!');
+      // leave while() loop if player is dead
       break;
     } else {
       window.alert(playerInfo.name + ' still has ' + playerInfo.health + ' health left.');
     }
-  }
-};
+  } // end of while loop
+}; // end of fight function
 
-
+// function to start a new game
 var startGame = function() {
-
+  // reset player stats
   playerInfo.reset();
 
- 
+  // fight each enemy robot by looping over them and fighting them one at a time
   for (var i = 0; i < enemyInfo.length; i++) {
- 
+    // if player is still alive, keep fight next enemy
     if (playerInfo.health > 0) {
-     window.alert('Welcome to Robot Gladiators! Round ' + (i + 1));
+      // let player know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it
+      window.alert('Welcome to Robot Gladiators! Round ' + (i + 1));
 
-     var pickedEnemyObj = enemyInfo[i];
+      // pick new enemy to fight based on the index of the enemyInfo array
+      var pickedEnemyObj = enemyInfo[i];
 
-      pickedEnemyObj.health = randomNumber(40, 60)
+      // set health for picked enemy
+      pickedEnemyObj.health = randomNumber(40, 60);
 
-       fight(pickedEnemyObj);
+      // pass the pickedEnemyObj object variable's value into the fight function, where it will assume the value of the enemy parameter
+      fight(pickedEnemyObj);
 
+      // if player is still alive and we're not at the last enemy in the array
       if (playerInfo.health > 0 && i < enemyInfo.length - 1) {
-  
+        // ask if player wants to use the store before next round
         var storeConfirm = window.confirm("The fight is over, visit the store before the next round?");
-          
+      
+        // if yes, take them to the store() function
         if (storeConfirm) {
           shop();
         }
       }
-    } 
+    }
+    // if player is not alive, break out of the loop and let endGame function run
     else {
       window.alert("You have lost your robot in battle! Game Over!");
       break;
     }
   }
-   
+
+  // after loop ends, we are either out of player.health or enemies to fight, so run the endGame function
   endGame();
 };
 
-
+// function to end the entire game
 var endGame = function() {
   window.alert("The game has now ended. Let's see how you did!");
 
-
+  // if player is still alive, player wins!
   if (playerInfo.health > 0) {
     window.alert("Great job, you've survived the game! You now have a score of " + playerInfo.money + '.');
   } else {
     window.alert("You've lost your robot in battle!");
   }
 
-
+  // ask player if they'd like to play again
   var playAgainConfirm = window.confirm('Would you like to play again?');
 
   if (playAgainConfirm) {
@@ -115,11 +130,9 @@ var endGame = function() {
   }
 };
 
-
-
-
+// go to shop between battles function
 var shop = function() {
-
+  // ask player what they'd like to do
   var shopOptionPrompt = window.prompt(
     "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one 1 for REFILL, 2 for UPGRADE, or 3 for LEAVE."
   );
@@ -143,11 +156,9 @@ var shop = function() {
   }
 };
 
-var randomNumber = function(min, max) {
-  var value = Math.floor(Math.random() * (max - min + 1) + min);
+/* END GAME FUNCTIONS */
 
-  return value;
-};
+/* GAME INFORMATION / VARIABLES */
 
 var getPlayerName = function() {
   var name = "";
@@ -169,7 +180,7 @@ var playerInfo = {
     this.health = 100;
     this.money = 10;
     this.attack = 10;
-  }, // comma!
+  },
   refillHealth: function() {
     if (this.money >= 7) {
       window.alert("Refilling player's health by 20 for 7 dollars.");
@@ -192,21 +203,21 @@ var playerInfo = {
   }
 };
 
-var enemyHealth = 50;
-
+// enemy information
 var enemyInfo = [
   {
-    name: "Roborto",
+    name: 'Roborto',
     attack: randomNumber(10, 14)
   },
   {
-    name: "Amy Android",
+    name: 'Amy Android',
     attack: randomNumber(10, 14)
   },
   {
-    name: "Robo Trumble",
+    name: 'Robo Trumble',
     attack: randomNumber(10, 14)
   }
 ];
+
 
 startGame();
